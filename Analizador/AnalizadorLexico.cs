3adeaa474321c362888c20480fabc2883da3fd1f
USUARIO <USUARIO@LAPTOP-BH0QBSA3>
2020-08-20 23:56:12 -0600

    Agregar archivos de proyecto.

---
+++ Analizador/AnalizadorLexico.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Analizador
+{
+    class AnalizadorLexico
+    {
+        private LinkedList<Token> Salida;
+        private int estado;
+        private String lexema;
+
+        public object MessageBoxButtons { get; private set; }
+
+        public LinkedList<Token> escanear(String entrada)
+        {
+            entrada = entrada + "#";
+            Salida = new LinkedList<Token>();
+            estado = 0;
+            lexema = "";
+            Char variable;
+
+            for (int i = 0; i <= entrada.Length - 1; i++)
+            {
+                //caracter que voy a leer. 
+                variable = entrada.ElementAt(i);
+
+                switch (estado)
+                {
+                    case 0:
+                        if (Char.IsDigit(variable))
+                        {
+                            estado = 1;
+                            lexema += variable;
+                        }
+                        else if (variable.CompareTo('Q') == 0)
+                        {
+                            lexema += variable;
+                            AgregarToken(Token.Tipo.MONEDA);
+
+                        }
+                        break;
+                    case 1:
+                        if (Char.IsDigit(variable))
+                        {
+                            estado = 1;
+                            lexema += variable;
+                        }
+                        else if (variable.CompareTo('.') == 0)
+                        {
+                            estado = 2;
+                            lexema += variable;
+                        }
+                        else
+                        {
+                            AgregarToken(Token.Tipo.NUMERO_ENTERO);
+                            i -= 1;
+                        }
+                        break;
+
+
+                    case 2:
+                        if (Char.IsDigit(variable))
+                        {
+                            estado = 2;
+                            lexema += variable;
+                        }
+                        else
+                        {
+                            AgregarToken(Token.Tipo.NUMERO_DECIMAL);
+                            i -= 1;
+                        }
+                        break;
+
+                    case 3:
+                        if (Char.IsLetter(variable))
+                        {
+                            estado = 3;
+                            lexema += variable;
+                        }
+                        else if (Char.IsLetter(variable))
+                        {
+                            lexema += variable;
+                            AgregarToken(Token.Tipo.IDENTIFICADOR);
+
+                        }
+                        break;
+                }
+
+            }
+            return Salida;
+        }
+
+        private void AgregarToken(Token.Tipo tipo)
+        {
+            Salida.AddLast(new Token(tipo, lexema));
+            lexema = "";
+            estado = 0;
+        }
+
+        public void imprimirListaToken(LinkedList<Token> lista)
+        {
+            foreach (Token item in lista)
+            {
+                MessageBox.Show(item.GetTipo() + "<-->" + item.Getvalor());
+            }
+
+        }
+    }
+}
+
---
+++ Analizador/Token.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analizador
+{
+    class Token
+    {
+        public enum Tipo
+        {
+            NUMERO_ENTERO,
+            NUMERO_DECIMAL,
+            MONEDA,
+            IDENTIFICADOR
+
+        }
+
+        private Tipo TipoToken;
+        private String valor;
+
+        public Token(Tipo tipotoken, String valor)
+        {
+            this.TipoToken = tipotoken;
+            this.valor = valor;
+        }
+
+        public String Getvalor()
+        {
+            return valor;
+        }
+
+        public String GetTipo()
+        {
+            switch (TipoToken)
+            {
+                case Tipo.IDENTIFICADOR:
+                    return "Identificador";
+                case Tipo.MONEDA:
+                    return "Moneda";
+                case Tipo.NUMERO_DECIMAL:
+                    return "Numero Decimal";
+                case Tipo.NUMERO_ENTERO:
+                    return "Numero Entero";
+                default:
+                    return "Desconocido";
+
+
+
+
+
+            }
+        }
+
+    }
+}
+
